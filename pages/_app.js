1ec4be6591930607853f4a959b27b82d2a12529a
ajay budaniya <ajayasrdeveloper1@gmail.com>
2026-09-15 13:05:23 +0530

    added

--- pages/_app.js
+++ pages/_app.js
@@ -6,22 +6,36 @@ import { CartProvider } from "../src/component/CartContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-          <ToastContainer position="top-right" />
+      <ToastContainer position="top-right" />
 
       <Head>
         <title>Budaniya Technologies LLP</title>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9178494129435619"
+          crossorigin="anonymous"
+        ></script>
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8RQH9FSEW6"
+        ></script>
+
+        <script>
+          window.dataLayer = window.dataLayer || [] function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-8RQH9FSEW6');
+        </script>
       </Head>
       <CartProvider>
-      <Navbar />
-      <Component {...pageProps} />
+        <Navbar />
+        <Component {...pageProps} />
       </CartProvider>
       <Footer />
-      
     </>
   );
 };
