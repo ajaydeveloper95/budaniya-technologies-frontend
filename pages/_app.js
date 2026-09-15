9229bbbdfa5b01c70b338908f519c4e3d2c5c799
ajay budaniya <ajayasrdeveloper1@gmail.com>
2026-09-15 13:47:58 +0530

    added

--- pages/_app.js
+++ pages/_app.js
@@ -10,42 +10,45 @@ import Script from "next/script";
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
-      <ToastContainer position="top-right" />
-
       <Head>
         <title>Budaniya Technologies LLP</title>
         <link rel="icon" href="/favicon.ico" />
-
-        {/* Google AdSense */}
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9178494129435619"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
-
-        {/* Google Analytics */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-8RQH9FSEW6"
-          strategy="afterInteractive"
-        />
-
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-8RQH9FSEW6');
-          `}
-        </Script>
       </Head>
+
+      {/* Google AdSense */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9178494129435619"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+
+      {/* Google Analytics */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-8RQH9FSEW6"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8RQH9FSEW6');
+        `}
+      </Script>
+
+      <ToastContainer position="top-right" />
+
       <CartProvider>
         <Navbar />
         <Component {...pageProps} />
       </CartProvider>
+
       <Footer />
     </>
   );
 };
+
 export default MyApp;
